15c2f08ad673cc8eeed7e8665bf1b6b55b748ca2
Mohammad Raj <122721728+S4BB1R-69@users.noreply.github.com>
2023-02-28 10:43:40 +0600

    Update S4X.py

--- S4X.py
+++ S4X.py
@@ -261,7 +261,7 @@ def rcrack(uid,pwx,tl):
     'sec-fetch-user': '?1',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Mobile; rv:48.0; A405DL) Gecko/48.0 Firefox/48.0 KAIOS/2.5',
-,}
+}
 			lo = session.post(''https://free.facebook.com/'',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
 			if 'c_user' in log_cookies:
